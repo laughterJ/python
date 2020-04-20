319150d147ef24dea7c7f6803a18ec39cc964c2a
CCCode <jianghao_code@163.com>
2020-04-20 10:17:14 +0800

    函数

--- code_7.py
+++ code_7.py
@@ -1,5 +1,18 @@
 """函数"""
 
+# 导入整个模块
+import code_8
+# 导入特定的函数
+from code_8 import print_info_1
+# 使用 ad 给模块指定别名
+import code_8 as p
+# 使用 as 给函数指定别名
+from code_8 import print_info_2 as pi
+# 导入模块中所有函数
+from code_8 import *
+
+
+
 # 定义函数
 def hello():
 	print("Hello python")
@@ -35,3 +48,8 @@ def format_name(first_name, last_name):
 print(format_name("Hao", "Jiang"))
 
 
+# 将函数存储在模块中
+code_8.print_info(["name", "age", "sex", "grade"])
+print_info_1(["name", "age", "sex", "grade"])
+pi(["name", "age", "sex", "grade"])
+p.print_info_2(["Jack", 18, "male", 7])
---
+++ code_8.py
@@ -0,0 +1,12 @@
+def print_info(info):
+	while info:
+		print(info.pop())
+
+
+def print_info_1(info):
+	for ele in info:
+		print(ele)
+
+
+def print_info_2(info):
+	print(info)
